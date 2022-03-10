49d0a6a0c795c090e999f013558b25e1927ea1ff
daliudzius <daliudzius@gmail.com>
2022-03-10 14:57:53 -0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,12 @@ export default function Footer() {
   return (
     <Center w={"100%"} pt={8}>
       <hr />
-      Built with Hasura backend. |{" "}
-      <a href="https://github.com/daliudzius/rocket-conversion">Source code</a>
+      Built with Hasura backend. |
+      <a href="https://github.com/daliudzius/rocket-conversion">
+        {" "}
+        Source code{" "}
+      </a>
+      | Dannick Liudzius
     </Center>
   )
 }

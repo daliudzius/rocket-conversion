f13771d5de039d3c40089ad2800774124e12ddde
daliudzius <daliudzius@gmail.com>
2022-03-10 14:44:09 -0800

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import GithubProvider from "next-auth/providers/github"
-import Auth0Provider from "next-auth/providers/auth0"
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
 export default NextAuth({
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
@@ -13,11 +9,12 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   theme: {
     colorScheme: "light",
   },
   callbacks: {
-    async jwt({ token, isNewUser, account }) {
+    async jwt({ token }) {
       token.userRole = "admin"
       return token
     },

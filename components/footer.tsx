b5980bd0178486ce395445bdf94ff03c2fa7bab1
daliudzius <daliudzius@gmail.com>
2022-03-10 15:01:37 -0800

    add footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,18 @@
-import { Center } from "@chakra-ui/react"
+import { Center, Link } from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
 
 export default function Footer() {
   return (
     <Center w={"100%"} pt={8}>
       <hr />
-      Built with Hasura backend. |
-      <a href="https://github.com/daliudzius/rocket-conversion">
-        {" "}
-        Source code{" "}
-      </a>
-      | Dannick Liudzius
+      Built with Hasura backend. |{" "}
+      <Link href="https://github.com/daliudzius/rocket-conversion" isExternal>
+        Source code <ExternalLinkIcon mx="2px" />
+      </Link>{" "}
+      |{" "}
+      <Link href="https://www.dannick.dev/" isExternal>
+        @Dannick Liudzius
+      </Link>
     </Center>
   )
 }
